d80ff17877f820e8dd788fe798572758fc359d65
Raphpat <rpattiecsi@gmail.com>
2018-02-13 21:26:52 +0100

    Letter input and treatment that works!

--- HowToHangman.py
+++ HowToHangman.py
@@ -1,58 +1,49 @@
+#11 erreurs pour perdre
 mot = "hey"
 interdite =[]
 vide = ["_" for i in range(len(mot))]
-erreur = True
+fautes = 0
 
-def choixLettre(mot,vide,interdite,erreur):
-    while True:
-        try:
-            while True:
-                lettre = str(input("Choisissez une lettre: "))
-                if len(lettre) == 1 and lettre.isalpha():
-                    break
-                print("Veuillez rentrer qu'une seule lettre!")
-            break
-        except:
-            print("Veuillez rentrer une lettre!")
-    for i in range(len(mot)):
-        if lettre == mot[i] and lettre != vide[i]:
-            vide[i] = lettre
-            erreur = False
-        elif lettre == vide[i] or lettre == interdite:
-            print("Vous avez déjà utilisé cette lettre")
-            break
+def entre():
+    statut = True
+    while statut == True:
+        lettre = str(input("Choisissez une lettre: "))
+        if len(lettre) == 1 and lettre.isalpha():
+            statut = False
         else:
-            interdite.append(lettre)
-    return(erreur, vide, interdite)
+            print("Veuillez rentrer qu'une seule lettre!")
+    return(lettre)
 
-print(mot)
-print(len(mot))
-print(vide)
-while True:
-    tada = choixLettre(mot,vide,interdite,erreur)
-    erreur = tada[0]
-    vide = tada[1]
-    interdite = tada[2]
-    print(erreur)
-    print(vide)
-    print("Lettres utilisées: ",interdite)
+#change input to take from the GUI
 
-"""
-mot = "bonjour"
-print(mot[3])
-find = ["_" for i in range(len(mot))]
-print(" ".join(find))
-echec = 0
-while echec < 11:
-    lettre = str(input("Devinez une lettre: "))
-    statut = False
+def traitement():
+    global fautes
+    lettre = entre()
+    erreur = True
     for i in range(len(mot)):
-        if mot[i]== lettre:
-            find[i] = lettre
-            statut = True
-    if statut == False:
-        echec +=1
-    print(" ".join(find))
-    statut = False
-    print(echec)
-"""
+        if lettre in interdite:
+            print("Vous avez déjà utiliser cette lettre!")
+            break
+        elif lettre == mot[i]:
+            erreur = False
+            vide[i] = mot[i]
+    if erreur == True:
+        interdite.append(lettre)
+        fautes +=1
+
+print(vide)
+
+while True:
+    traitement()
+    if fautes == 11:
+        print("C'est perdu!")
+        break
+    print("vide: ", vide)
+    print("interdite: ", interdite)
+    count = 0
+    for i in range(len(mot)):
+        if vide[i] != '_':
+            count += 1
+    if count == len(mot):
+        print("Victory!")
+        break
