93bfdd871c3f04bcffcef9d29cc53b388965a3b0
LucyEberhardt <35577801+LucyEberhardt@users.noreply.github.com>
2018-02-25 17:40:54 +0100

    Add files via upload

---
+++ motspourpendu.py
@@ -0,0 +1,11 @@
+mots_pour_pendu = {niveau 1 = {français = {1:"chat", 2:"lait", 3:"viande", 4:"lit", 5:"chaise", 6:"veste", 7:"boite", 8:"train", 9:"bonbon", 10:"sac"}
+                              anglais = {1:"cat", 2:"milk", 3:"meat", 4:"bed", 5:"chair", 6:"coat", 7:"box", 8:"train", 9:"sweet", 10:"bag"}}
+                               
+                   niveau 2 = {français = {1:"chapeau", 2:"cuisine", 3:"tiroire", 4:"avoca", 5:"crepe", 6:"lumière", 7:"telephone", 8:"volets", 9:"magazine", 10:"plante"}
+                              anglais = {1:"hat", 2:"kitchin", 3:"draw", 4:"avocado", 5:"pancake", 6:"light", 7:"telephone", 8:"shutters", 9:"magasine", 10:"plant"}}
+
+                   niveau 3 = {français = {1:"caramel", 2:"oreiller", 3:"ski-nautique", 4:"aviron", 5:"armoire", 6:"interieur", 7:"meuble", 8:"matelas", 9:"peluche", 10:"plafond"}
+                               anglais = {1:"caramel", 2:"pillow", 3:"water-skiing", 4:"rowing", 5:"cupboard", 6:"inside", 7:"furniture", 8:"mattress", 9:"soft-toy", 10:"ceiling"}}
+
+                   X-pert = {français = {1:"bibliothèque", 2:"dictionnaire", 3:"magnétoscope", 4:"orchestre", 5:"xylophone", 6:"zero", 7:"chuchotter", 8:"arrosoir", 9:"entonnoir", 10:"anticonstitutionnellement"}
+                             anglais = {1:"library", 2:"dictionary", 3:"video-recorder", 4:"orchestra", 5:"xylophone", 6:"zero", 7:"wisper", 8:"watering can", 9:"funnel", 10:"unconstitutionally"}}}
